a54c269dfb572dfd889dc3d7020124c505c8ff51
유성(yuseong) <62369592+yuseong01@users.noreply.github.com>
2025-06-08 21:20:46 +0900

    feat: Enemy생성, 죽음

--- Assets/Scripts/Character/Enemy.cs
+++ Assets/Scripts/Character/Enemy.cs
@@ -7,20 +7,39 @@ public class Enemy : StateMachine
     [Header("Enemy Stat")]
     [SerializeField] private float moveSpeed = 2f;
     [SerializeField] private float attackRange = 1.5f;
+    [SerializeField] private int maxHP = 50;
     
     public float MoveSpeed => moveSpeed;
     public float AttackRange => attackRange;
+    public int CurrentHP { get; private set; }
 
     [HideInInspector] public EnemyIdleState idleState;
-    //[HideInInspector] public EnemyChaseState chaseState;
-    //[HideInInspector] public EnemyAttackState attackState;
+    [HideInInspector] public EnemyChaseState chaseState;
+    [HideInInspector] public EnemyAttackState attackState;
 
     public void Start()
     {
+        CurrentHP = maxHP;
+        
         idleState = new EnemyIdleState(this);
         //chaseState = new EnemyChaseState(this);
         //attackState = new EnemyAttackState(this);
         
         ChangeState(idleState);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        CurrentHP -= damage;
+        if (CurrentHP <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Enemy Dead");
+        Destroy(gameObject);
     }
 }
