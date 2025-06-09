d9f1b6fc79b28718ca5551b1a54947eee4ac0320
유성(yuseong) <62369592+yuseong01@users.noreply.github.com>
2025-06-10 03:52:35 +0900

    refactor: 코루틴, OverlapSphereNonAlloc을 통한 최적화

--- Assets/Scripts/CharacterStates/PlayerStates/PlayerAttackState.cs
+++ Assets/Scripts/CharacterStates/PlayerStates/PlayerAttackState.cs
@@ -12,51 +12,91 @@ public class PlayerAttackState : IState
     private float attackCooldown = 1f;
     private float lastAttackTime;
 
+    private int enemyLayerMask;
+
+    private Coroutine detectCoroutine;
+    private WaitForSeconds waitForSeconds;
+    
+    Collider[] enemyColliders = new Collider[10];
+
+    
     public PlayerAttackState(Player player)
     {
         this.player = player;
         this.playerTransform = player.CachedTransform;
         this.attackRange = player.AttackRange;
+        
+        waitForSeconds = new WaitForSeconds(0.2f); 
+
+        enemyLayerMask = LayerMask.GetMask("Enemy");
     }
 
     public void Enter()
     {
         Debug.Log("PlayerAttack State Enter");
+
+        detectCoroutine = player.StartCoroutine(DetectEnemyCoroutine());
+        
     }
 
     public void Tick()
     {
-        Debug.Log("때리는중");
-        //범위 안에 들어온 Collider배열에 담아줌(OverlapSphere(position, radius, layerMask)
-        Collider[] hits = Physics.OverlapSphere(playerTransform.position, attackRange, LayerMask.GetMask("Enemy"));
-        Debug.Log("적 감지 개수:"+hits.Length);
         
-        if (hits.Length > 0)
+    }
+
+    private IEnumerator DetectEnemyCoroutine()
+    {
+        while (true)
         {
-            //한마리만 테스트
-            Enemy enemy = hits[0].GetComponent<Enemy>();
-            if (enemy == null)
+            AttackDetectedEnemy();
+            yield return waitForSeconds;
+        }
+    }
+    
+    private void AttackDetectedEnemy()
+    {
+        Debug.Log("때리는중");
+        
+        Enemy enemy = DetectEnemy();
+        
+        if (enemy != null)
+        {
+            if (Time.time - lastAttackTime > attackCooldown)
             {
-                Debug.Log("Enemy컴포넌트를 못찾음");
-            }
-            if (enemy != null)
-            {
-                if (Time.time - lastAttackTime > attackCooldown)
-                {
-                    enemy.TakeDamage(attackDamage);
-                    Debug.Log("Player attack enemy");
-                    lastAttackTime = Time.time;
-                }
+                enemy.TakeDamage(attackDamage);
+                Debug.Log("Player attack enemy");
+                lastAttackTime = Time.time;
             }
         }
         else
         {
             player.ChangeState(player.moveState);
         }
+        
     }
+    
+    private Enemy DetectEnemy() 
+    {
+        //collider배열을 미리 만들어놓고 거기에 덮어쓰기 방식으로 결과를 채워줘서 새로운 메모리 할당X -> GC Alloc 0
+        int count = Physics.OverlapSphereNonAlloc(playerTransform.position, attackRange, enemyColliders, enemyLayerMask);
+        // Debug.Log("적 감지 개수:"+hits.Length);
+        
+        if (count > 0)
+        {
+            //한마리만 테스트
+            return enemyColliders[0].GetComponent<Enemy>();  
+        }
 
+        return null;
+    }
+    
     public void Exit()
     {
         Debug.Log("Player Attack State Exit");
+
+        if (detectCoroutine != null)
+        {
+            player.StopCoroutine(detectCoroutine);
+        }
     }
 }
