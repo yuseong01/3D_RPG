7f3b57897675f2b8ba996e73df7f62d352c51bbe
유성(yuseong) <62369592+yuseong01@users.noreply.github.com>
2025-06-10 15:08:25 +0900

    feat: player AI

--- Assets/Scripts/Character/Player.cs
+++ Assets/Scripts/Character/Player.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : StateMachine
 {
     [Header("Player Stat")]
     [SerializeField] private float moveSpeed = 3f;
     [SerializeField] private float attackRange = 2f;
+    [SerializeField] private float detectRange = 5f;
+    private NavMeshAgent agent;
     
     public float MoveSpeed => moveSpeed;
     public float AttackRange => attackRange;
+    public float DetectRange => detectRange;
+    public NavMeshAgent Agent => agent;
     public Transform CachedTransform { get; private set; }
 
+    
     //[HideInInspector] public PlayerIdleState idleState;
     [HideInInspector] public PlayerMoveState moveState;
     [HideInInspector] public PlayerAttackState attackState;
@@ -22,10 +28,13 @@ public class Player : StateMachine
     }
     public void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = moveSpeed;
+        
         //idleState = new PlayerIdleState(this);
         moveState = new PlayerMoveState(this);
         attackState = new PlayerAttackState(this);
-
+        
         ChangeState(moveState);
     }
 
--- Assets/Scripts/CharacterStates/PlayerStates/PlayerMoveState.cs
+++ Assets/Scripts/CharacterStates/PlayerStates/PlayerMoveState.cs
@@ -1,33 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerMoveState : IState
 {
     private Player player;
     private Transform playerTransform;
-    private float moveSpeed;
+    private NavMeshAgent agent;
+    
+    private float attackRange;
+    private float detectRange;
+    private float lastWanderTime;
+    private float wanderCooldown = 2f;
+
+    private int enemyLayerMask;
+    private Collider[] enemyColliders = new Collider[10];
 
     public PlayerMoveState(Player player)
     {
         this.player = player;
         this.playerTransform = player.CachedTransform;
-        this.moveSpeed = player.MoveSpeed;
+        this.agent = player.Agent;
+        this.attackRange = player.AttackRange;
+        this.detectRange = player.DetectRange;
+
+        this.enemyLayerMask = LayerMask.GetMask("Enemy");
     }
 
     public void Enter()
     {
         Debug.Log("PlayerMoveState Enter");
+        Wander();
+        lastWanderTime = Time.time;
     }
 
     public void Tick()
     {
-        playerTransform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-        
-        Collider[] hits = Physics.OverlapSphere(playerTransform.position, player.AttackRange, LayerMask.GetMask("Enemy"));
-        if (hits.Length > 0)
+        float now = Time.time;
+
+        int count = Physics.OverlapSphereNonAlloc(playerTransform.position, detectRange, enemyColliders, enemyLayerMask);
+
+        if (count > 0)
         {
-            player.ChangeState(player.attackState);
+            Transform target = enemyColliders[0].transform;
+            agent.SetDestination(target.position);
+
+            float sqrDist = (target.position - playerTransform.position).sqrMagnitude;
+            if (sqrDist <= attackRange * attackRange)
+            {
+                player.ChangeState(player.attackState);
+                return;
+            }
+        }
+        else
+        {
+            if (now - lastWanderTime > wanderCooldown)
+            {
+                Wander();
+                lastWanderTime = now;
+            }
         }
     }
 
@@ -35,4 +67,20 @@ public class PlayerMoveState : IState
     {
         Debug.Log("PlayerMoveState Exit");
     }
+
+    private void Wander()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * Random.Range(3f, 6f);
+        randomDirection += playerTransform.position;
+        
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomDirection, out hit, 2f, NavMesh.AllAreas))
+        {
+            agent.SetDestination(hit.position);
+        }
+        
+        lastWanderTime = Time.time;
+    }
 }
+
+    
