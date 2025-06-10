20963494f66b32de1727b262060f71d4c045d600
유성(yuseong) <62369592+yuseong01@users.noreply.github.com>
2025-06-10 18:40:16 +0900

    feat: UIManager

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIManager : MonoBehaviour
+public class UIManager : Singleton<UIManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject uiMainMenu;
+    [SerializeField] private GameObject uiStatus;
+    [SerializeField] private GameObject uiInventory;
+
+    public void OpenMainMenu()
     {
-        
+        uiMainMenu.SetActive(true);
+        uiStatus.SetActive(false);
+        uiInventory.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenStatus()
     {
-        
+        uiMainMenu.SetActive(false);
+        uiStatus.SetActive(true);
+    }
+
+    public void OpenInventory()
+    {
+        uiMainMenu.SetActive(false);
+        uiInventory.SetActive(true);
     }
 }
---
+++ Assets/Scripts/Utility/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{   
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = (T)FindObjectOfType(typeof(T));
+                if (_instance == null)
+                {
+                    GameObject singletonObject = new GameObject();
+                    _instance = singletonObject.AddComponent<T>();
+                    singletonObject.name = typeof(T).ToString() + " (Singleton)";
+                    DontDestroyOnLoad(singletonObject);
+                }
+            }
+            return _instance;
+        }
+    }
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
